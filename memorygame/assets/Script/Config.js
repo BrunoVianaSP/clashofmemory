07fa3c7fdb1f66cda72e7d361570d4b051d0c97b
Viana <brunovianasp@gmail.com>
2018-07-10 15:23:18 -0300

    Separate Continents

---
+++ memorygame/assets/Script/Config.js
@@ -0,0 +1,23 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        continents: {
+            // ATTRIBUTES:
+            south_america: ["Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"],
+            africa: [],
+            europe: [],
+            asia: [],
+            oceanian: []
+
+        }
+    },
+
+    // onLoad () {},
+
+    start() {
+
+    },
+
+    // update (dt) {},
+});
